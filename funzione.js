97a07c4d6fd8ab52e29cb135d85084a6aa8032b2
Narderio <nardelladario2003@gmail.com>
2021-05-28 09:15:44 +0200

    primo

---
+++ funzione.js
@@ -0,0 +1,17 @@
+
+
+
+
+function controlla()
+{
+  var a=document.getElementById("testo");
+
+  if(a.value=="")
+      alert("Il campo e' vuoto");
+
+
+ else
+    alert("Il contenuto e' " + a.value);
+
+
+  }
